90166e0fa804f1ae6092c01adb0ea214476e411e
datschi1 <97352464+datschi1@users.noreply.github.com>
2022-01-08 19:57:15 +0400

    Add files via upload

---
+++ coverletter.py
@@ -0,0 +1,8 @@
+name = input("Your name: ")
+name = name.title()
+position = input("The Position")
+
+
+print("Dear Hiring Manager, \n"f"I would like to express my interest in the {position} position")
+print("I think I would be an excellent fit for a wide range of reasons.\n""I have specific industry experience that strongly aligns with the day-to-day operations of , and of the role specifically.")
+print("I would appreciate an opportunity to discuss the role and what value I could bring to you.\n"f"{name}")
